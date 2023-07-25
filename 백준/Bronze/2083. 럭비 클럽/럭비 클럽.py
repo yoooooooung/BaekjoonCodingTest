e1e61a513f0859570a0788ff4862e431b23d886c
Jinyoung C <113872386+yoooooooung@users.noreply.github.com>
2023-07-25 17:48:29 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,10 @@
+while 1:
+    name, age, weight = input().split()
+    if name == "#":
+        break
+    else:
+        # print(f'{name} Senior') if (int(age) > 17 or int(weight) >= 80) else print(f'{name} Junior')
+        if int(age) > 17 or int(weight) >= 80:
+            print(f'{name} Senior')
+        else:
+            print(f'{name} Junior')
